cf8e2eeb0c3b1db819855fe78c00b6d6188aa6c3
jaypms <beatsmarket.best@gmail.com>
2025-07-20 08:13:43 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,5 +4,5 @@ export default function Home() {
       <h1 className="text-4xl font-bold mb-4">Bienvenue sur BeatsMarket 🎧</h1>
       <p className="text-lg">Ceci est la page d’accueil. Tout fonctionne bien 🎉</p>
     </main>
-  );
+  )
 }

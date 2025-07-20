64adf723703ca5946cccfea84c98e2b0d624c510
jaypms <beatsmarket.best@gmail.com>
2025-07-20 08:10:04 +0200

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-8 text-white bg-black min-h-screen">
+      <h1 className="text-4xl font-bold mb-4">Bienvenue sur BeatsMarket 🎧</h1>
+      <p className="text-lg">Ceci est la page d’accueil. Tout fonctionne bien 🎉</p>
+    </main>
+  );
+}

94af1cf24e287ddd106117c44c8fb64a7d277f1a
jaypms <beatsmarket.best@gmail.com>
2025-07-22 00:58:15 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,16 @@
-import type { Config } from "tailwindcss"
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}"
+    './app/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#D33F8D",  // rose vif BeatsMarket
-        secondary: "#6F42C1", // violet BeatsMarket
-        darkbg: "#121212",
-        darkbg2: "#181818"
-      },
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-        ptsans: ["PT Sans", "sans-serif"]
-      }
-    }
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
 
 export default config

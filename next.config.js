94d5c5a62242c376ff4d87102a74881ec41fdd31
jaypms <beatsmarket.best@gmail.com>
2025-07-22 01:37:36 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    // tu peux activer ici des options expérimentales si besoin
-  },
-  // autres options possibles...
 }
 
 module.exports = nextConfig

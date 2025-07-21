ce1db6d97b5ffe19439660d2498c66ae5474e8b1
jaypms <beatsmarket.best@gmail.com>
2025-07-21 23:42:48 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    './app/**/*.{js,ts,jsx,tsx}',   // pour Next.js app directory
+    './pages/**/*.{js,ts,jsx,tsx}', // si tu as encore un dossier pages
+    './components/**/*.{js,ts,jsx,tsx}', // tes composants
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: {
+          400: '#FF6FAF',
+          500: '#FF3F8B',
+        },
+        violet: {
+          400: '#8B5CF6',
+          500: '#7C3AED',
+        },
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        serif: ['PT Serif', 'serif'],
+      },
+    },
   },
   plugins: [],
 }

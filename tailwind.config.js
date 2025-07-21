a2f51bb4d48732210c18df977567b880042a790a
jaypms <beatsmarket.best@gmail.com>
2025-07-22 00:54:20 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", 
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    './app/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#ff007f', // rose vif BeatsMarket
-        secondary: '#7c3aed', // violet
-        background: '#0f0f0f', // fond sombre
-        text: '#eee'
-      },
-      fontFamily: {
-        inter: ['var(--font-inter)', 'sans-serif']
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }

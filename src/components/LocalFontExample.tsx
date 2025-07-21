bdc448ef5d67bd0fda1ab7499b44a8f2b747b27c
jaypms <beatsmarket.best@gmail.com>
2025-07-22 01:25:19 +0200

    Update LocalFontExample.tsx

--- src/components/LocalFontExample.tsx
+++ src/components/LocalFontExample.tsx
@@ -1,28 +1,23 @@
 import localFont from 'next/font/local'
 
 const myFont = localFont({
-  src: [
-    {
-      path: '../fonts/MyFont-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/MyFont-Bold.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
+  src: '../fonts/MyFont-Regular.woff2',
   variable: '--my-font',
   display: 'swap',
 })
 
 export default function LocalFontExample() {
   return (
-    <div className={myFont.variable}>
-      <p style={{ fontFamily: 'var(--my-font)' }}>
-        Ceci est un texte utilisant une font locale avec next/font/local.
-      </p>
-    </div>
+    <>
+      <style jsx global>{`
+        :root {
+          --my-font: ${myFont.style.fontFamily};
+        }
+      `}</style>
+      <main className={myFont.className}>
+        <h1>Exemple de police locale avec Next.js</h1>
+        <p>Cette page utilise une police locale avec la variable CSS {`--my-font`}.</p>
+      </main>
+    </>
   )
 }

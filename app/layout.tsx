2d23a34f252e7ddbf3bdd0120d531a6fe68e19c9
jaypms <beatsmarket.best@gmail.com>
2025-07-21 20:43:01 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,9 @@
-import './globals.css'
-import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import '../src/styles/globals.css'
 
-export const metadata = {
-  title: 'BeatsMarket - La plateforme mondiale des beats',
-  description: 'Achetez, vendez et découvrez les meilleurs beats musicaux.',
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body className="bg-darkbg text-white font-poppins flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">{children}</main>
-        <Footer />
-      </body>
+      <body>{children}</body>
     </html>
   )
 }

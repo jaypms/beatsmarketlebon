2a1753b00c03ea65eafa26043950aa69a09a3e11
jaypms <beatsmarket.best@gmail.com>
2025-07-22 01:26:44 +0200

    Create index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import LocalFontExample from '@/components/LocalFontExample'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur BeatsMarket</h1>
+      <LocalFontExample />
+    </div>
+  )
+}

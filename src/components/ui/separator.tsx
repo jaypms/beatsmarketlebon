35e82becea0814079c27ebccc7a38318772f3600
jaypms <beatsmarket.best@gmail.com>
2025-07-21 19:57:04 +0200

    Update separator.tsx

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,17 +1,7 @@
 import * as React from "react"
 
-interface SeparatorProps extends React.HTMLAttributes<HTMLHRElement> {}
-
-export const Separator = React.forwardRef<HTMLHRElement, SeparatorProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <hr
-        ref={ref}
-        className={`border-gray-300 dark:border-gray-700 my-4 ${className ?? ""}`}
-        {...props}
-      />
-    )
-  }
-)
-
-Separator.displayName = "Separator"
+export function Separator() {
+  return (
+    <hr className="border-t border-gray-200 my-4 dark:border-gray-700" />
+  )
+}

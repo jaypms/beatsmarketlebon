ae4949adca818d0ae759aa63b12560aa9c5f4bab
jaypms <beatsmarket.best@gmail.com>
2025-07-21 19:55:55 +0200

    Update toggle.tsx

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -1,22 +1,46 @@
 import * as React from "react"
 
-interface ToggleProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface ToggleProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  checked?: boolean
+  onCheckedChange?: (checked: boolean) => void
+}
 
-export const Toggle = React.forwardRef<HTMLInputElement, ToggleProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <label className={`inline-flex relative items-center cursor-pointer ${className ?? ""}`}>
-        <input
-          type="checkbox"
-          className="sr-only peer"
-          ref={ref}
-          {...props}
-        />
-        <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:bg-primary transition-colors duration-300"></div>
-        <div className="absolute left-1 top-1 w-4 h-4 bg-white rounded-full shadow-md peer-checked:translate-x-5 transition-transform duration-300"></div>
-      </label>
-    )
+export function Toggle({ checked, onCheckedChange, ...props }: ToggleProps) {
+  const [isChecked, setIsChecked] = React.useState(checked ?? false)
+
+  React.useEffect(() => {
+    if (checked !== undefined) {
+      setIsChecked(checked)
+    }
+  }, [checked])
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(e.target.checked)
+    if (onCheckedChange) {
+      onCheckedChange(e.target.checked)
+    }
   }
-)
 
-Toggle.displayName = "Toggle"
+  return (
+    <label className="inline-flex items-center cursor-pointer">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleChange}
+        className="sr-only"
+        {...props}
+      />
+      <span
+        className={`w-10 h-6 bg-gray-300 rounded-full relative
+          ${isChecked ? "bg-primary" : "bg-gray-300"}
+          transition-colors duration-200 ease-in-out`}
+      >
+        <span
+          className={`block w-4 h-4 bg-white rounded-full shadow transform
+            ${isChecked ? "translate-x-4" : "translate-x-0"}
+            transition-transform duration-200 ease-in-out`}
+        ></span>
+      </span>
+    </label>
+  )
+}

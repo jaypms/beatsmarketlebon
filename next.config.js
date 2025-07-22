809c4921ed9d793ecc8c04280a3ecf2175bcf45c
jaypms <beatsmarket.best@gmail.com>
2025-07-22 09:32:22 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    disableCssModulesLightningCss: true
+  },
   images: {
-    domains: ['yourdomain.com'], // adapte selon tes besoins
+    domains: ['yourdomain.com'],
   },
 }
 

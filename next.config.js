76417dcea5adda9a547fa2e2a3b07a99518e8116
jaypms <beatsmarket.best@gmail.com>
2025-07-22 02:05:37 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // La minification avec SWC est activée par défaut dans Next.js 15 et plus
-  // swcMinify: true, // <-- supprimée car plus reconnue
+  images: {
+    domains: ['yourdomain.com'], // adapte selon tes besoins
+  },
 }
 
 module.exports = nextConfig

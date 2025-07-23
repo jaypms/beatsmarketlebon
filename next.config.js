07c0797b63248c86130d9e34fd2cb389429c584b
jaypms <beatsmarket.best@gmail.com>
2025-07-23 18:21:09 +0200

    Mise à jour de la config et ajout de fichiers

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+<<<<<<< HEAD
   reactStrictMode: true,
   experimental: {
     disableCssModulesLightningCss: true
   },
+=======
+>>>>>>> 66524c8 (Mise à jour de la config et ajout de fichiers)
   images: {
     domains: ['yourdomain.com'],
   },
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   plugins: {
+<<<<<<< HEAD
     'tailwindcss/nesting': {},
     '@tailwindcss/postcss': {},
+=======
+>>>>>>> 66524c8 (Mise à jour de la config et ajout de fichiers)
     autoprefixer: {},
   },
 }

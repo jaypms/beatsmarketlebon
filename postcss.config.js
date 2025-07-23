bea6285248dc4ed87e533b25d49f7dbdd719918f
jaypms <beatsmarket.best@gmail.com>
2025-07-23 18:57:57 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   plugins: {
-<<<<<<< HEAD
-    'tailwindcss/nesting': {},
-    '@tailwindcss/postcss': {},
-=======
->>>>>>> 66524c8 (Mise à jour de la config et ajout de fichiers)
+    tailwindcss: {},
     autoprefixer: {},
   },
 }

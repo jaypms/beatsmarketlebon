01ff28b195483615061cf71ce251b82c66281a2a
jaypms <beatsmarket.best@gmail.com>
2025-07-24 09:19:18 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    optimizeCss: false, // On désactive Lightning CSS pour éviter les erreurs natives
+    // Désactive l'utilisation de lightningcss
+    swcPlugins: [],
+    optimizeCss: false,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

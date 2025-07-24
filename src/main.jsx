21ab5512b418078ae19ba64baf48d071d6624bf5
jaypms <beatsmarket.best@gmail.com>
2025-07-24 10:38:22 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
-// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/App.jsx'  // On va créer ce fichier aussi juste après
-import './styles/index.css'      // fichier CSS global, à créer ou adapter selon toi
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

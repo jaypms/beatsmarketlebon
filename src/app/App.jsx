21c135d0da5e2a2062b7a9f783c11ae7479e3e3c
jaypms <beatsmarket.best@gmail.com>
2025-07-24 10:33:00 +0200

    Create App.jsx

---
+++ src/app/App.jsx
@@ -0,0 +1,11 @@
+// src/app/App.jsx
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Bienvenue sur BeatsMarket</h1>
+      <p>Le projet est bien lancé !</p>
+    </div>
+  )
+}
